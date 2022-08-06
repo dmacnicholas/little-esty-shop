ffbe4a182a08dcee0a9781558d05d484c0c67c98
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-06 00:05:45 -0500

    Add test for link to show page and updated view

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -7,21 +7,26 @@ RSpec.describe 'discounts index page' do
 
     discount_1 = Discount.create!(percent: 25, threshold: 10, merchant_id: merchant_1.id)
     discount_2 = Discount.create!(percent: 15, threshold: 5, merchant_id: merchant_1.id)
-    discount_3 = Discount.create!(percent: 20, threshold: 10, merchant_id: merchant_2.id)
+    discount_3 = Discount.create!(percent: 20, threshold: 12, merchant_id: merchant_2.id)
 
     visit "/merchants/#{merchant_1.id}/discounts"
 
-    within("#discount-#{@disocunt_1.id}") do
-      expect(page).to have_content(@discount_1.percent)
-      expect(page).to have_content(@discount_1.threshold)
+    within("#discount-#{discount_1.id}") do
+      expect(page).to have_content(discount_1.percent)
+      expect(page).to have_content(discount_1.threshold)
+      expect(page).to have_link("View Discount #{discount_1.id}")
     end
 
-    within("#discount-#{@disocunt_2.id}") do
-      expect(page).to have_content(@discount_2.percent)
-      expect(page).to have_content(@discount_2.threshold)
+    within("#discount-#{discount_2.id}") do
+      expect(page).to have_content(discount_2.percent)
+      expect(page).to have_content(discount_2.threshold)
+      expect(page).to have_link("View Discount #{discount_2.id}")
     end
 
-    expect(page).to_not have_content(@discount_3.percent)
-    expect(page).to_not have_content(@discount_3.threshold)
+    expect(page).to_not have_content(discount_3.percent)
+    expect(page).to_not have_content(discount_3.threshold)
+    expect(page).to_not have_link("View Discount #{discount_3.id}")
+    click_link("View Discount #{discount_1.id}")
+    expect(current_path).to eq("/merchants/#{merchant_1.id}/discounts/#{discount_1.id}")
   end
 end
