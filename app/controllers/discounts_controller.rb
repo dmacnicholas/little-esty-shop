90f69f75c5cce364ee822d88b0e6087ed185b4f1
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-06 22:24:23 -0500

    Add create method for new discount

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -4,10 +4,26 @@ class DiscountsController < ApplicationController
   end
 
   def show
-
   end
 
   def new
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    discount = merchant.discounts.new(discount_params)
+    if discount.save
+      redirect_to "/merchants/#{merchant.id}/discounts"
+    else
+      redirect_to "/merchants/#{merchant.id}/discounts/new"
+      flash[:error] = "Error #{discount.errors.full_messages.join(", ")}"
+    end
+  end
+
+  private
+  def discount_params
+    params.permit(:percent, :threshold, :merchant_id)
+  end
+
 end
