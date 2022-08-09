96d8d553367577a1ff5f800ed9321d9c07f2af00
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-09 10:25:54 -0500

    Add tests for total discounts

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -92,6 +92,103 @@ RSpec.describe 'invoices show page' do
        expect(page).to have_content("pending")
      end
 
-    
+     it 'calculates that a bulk discount should not be applied to any items ' do
+       merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
+       item_1 = Item.create!(name: "Watch", description: "Always a need to tell time", unit_price: 30, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       item_2 = Item.create!(name: "Crocs", description: "Worst and Best Shoes", unit_price: 40, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       item_3 = Item.create!(name: "Beanie", description: "Perfect for a cold day", unit_price: 50, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       customer_1 = Customer.create!(first_name: "James", last_name: "Franco", created_at: Time.now, updated_at: Time.now)
+       invoice_1 = customer_1.invoices.create!(status: 1, created_at: Time.now, updated_at: Time.now)
+       invoice_item_1 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 5, unit_price: item_1.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       invoice_item_2 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_1.id, quantity: 5, unit_price: item_2.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       invoice_item_3 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_1.id, quantity: 5, unit_price: item_3.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       discount_1 = Discount.create!(percent: 20, threshold: 10, merchant_id: merchant_1.id)
+
+       visit "/merchants/#{merchant_1.id}/invoices/#{invoice_1.id}"
+
+       expect(page).to have_content("Total Revenue: $600")
+       expect(page).to have_content("Total Discounts: $0")
+       expect(page).to have_content("Net Revenue: $600")
+     end
+
+     it 'calculates that a bulk discount should be applied to some items but not all items' do
+       merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
+       item_1 = Item.create!(name: "Watch", description: "Always a need to tell time", unit_price: 30, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       item_2 = Item.create!(name: "Crocs", description: "Worst and Best Shoes", unit_price: 40, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       item_3 = Item.create!(name: "Beanie", description: "Perfect for a cold day", unit_price: 50, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       customer_1 = Customer.create!(first_name: "James", last_name: "Franco", created_at: Time.now, updated_at: Time.now)
+       invoice_1 = customer_1.invoices.create!(status: 1, created_at: Time.now, updated_at: Time.now)
+       invoice_item_1 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 10, unit_price: item_1.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       invoice_item_2 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_1.id, quantity: 5, unit_price: item_2.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       invoice_item_3 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_1.id, quantity: 5, unit_price: item_3.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       discount_1 = Discount.create!(percent: 20, threshold: 10, merchant_id: merchant_1.id)
+
+       visit "/merchants/#{merchant_1.id}/invoices/#{invoice_1.id}"
+
+       expect(page).to have_content("Total Revenue: $750")
+       expect(page).to have_content("Total Discounts: $60")
+       expect(page).to have_content("Net Revenue: $690")
+     end
+
+     it 'calculates that multiple bulk discounts should be applied to different items' do
+       merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
+       item_1 = Item.create!(name: "Watch", description: "Always a need to tell time", unit_price: 30, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       item_2 = Item.create!(name: "Crocs", description: "Worst and Best Shoes", unit_price: 40, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       item_3 = Item.create!(name: "Beanie", description: "Perfect for a cold day", unit_price: 50, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       customer_1 = Customer.create!(first_name: "James", last_name: "Franco", created_at: Time.now, updated_at: Time.now)
+       invoice_1 = customer_1.invoices.create!(status: 1, created_at: Time.now, updated_at: Time.now)
+       invoice_item_1 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 10, unit_price: item_1.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       invoice_item_2 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_1.id, quantity: 15, unit_price: item_2.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       invoice_item_3 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_1.id, quantity: 5, unit_price: item_3.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       discount_1 = Discount.create!(percent: 20, threshold: 10, merchant_id: merchant_1.id)
+       discount_2 = Discount.create!(percent: 30, threshold: 15, merchant_id: merchant_1.id)
+
+       visit "/merchants/#{merchant_1.id}/invoices/#{invoice_1.id}"
+
+       expect(page).to have_content("Total Revenue: $1150")
+       expect(page).to have_content("Total Discounts: $240")
+       expect(page).to have_content("Net Revenue: $910")
+     end
+
+     it 'calculates that multiple bulk discounts but uses the one with the higher percentage regardless of threshold' do
+       merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
+       item_1 = Item.create!(name: "Watch", description: "Always a need to tell time", unit_price: 30, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       item_2 = Item.create!(name: "Crocs", description: "Worst and Best Shoes", unit_price: 40, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       item_3 = Item.create!(name: "Beanie", description: "Perfect for a cold day", unit_price: 50, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       customer_1 = Customer.create!(first_name: "James", last_name: "Franco", created_at: Time.now, updated_at: Time.now)
+       invoice_1 = customer_1.invoices.create!(status: 1, created_at: Time.now, updated_at: Time.now)
+       invoice_item_1 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 10, unit_price: item_1.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       invoice_item_2 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_1.id, quantity: 15, unit_price: item_2.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       invoice_item_3 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_1.id, quantity: 5, unit_price: item_3.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       discount_1 = Discount.create!(percent: 20, threshold: 10, merchant_id: merchant_1.id)
+       discount_2 = Discount.create!(percent: 15, threshold: 15, merchant_id: merchant_1.id)
+
+       visit "/merchants/#{merchant_1.id}/invoices/#{invoice_1.id}"
+
+       expect(page).to have_content("Total Revenue: $1150")
+       expect(page).to have_content("Total Discounts: $180")
+       expect(page).to have_content("Net Revenue: $970")
+     end
+
+     it 'calculates that multiple bulk discounts for multiple merchants' do
+       merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
+       merchant_2 = Merchant.create!(name: "Revtrics", created_at: Time.now, updated_at: Time.now)
+       item_1 = Item.create!(name: "Watch", description: "Always a need to tell time", unit_price: 30, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       item_2 = Item.create!(name: "Crocs", description: "Worst and Best Shoes", unit_price: 40, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+       item_3 = Item.create!(name: "Beanie", description: "Perfect for a cold day", unit_price: 50, merchant_id: merchant_2.id, created_at: Time.now, updated_at: Time.now)
+       customer_1 = Customer.create!(first_name: "James", last_name: "Franco", created_at: Time.now, updated_at: Time.now)
+       invoice_1 = customer_1.invoices.create!(status: 1, created_at: Time.now, updated_at: Time.now)
+       invoice_item_1 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 10, unit_price: item_1.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       invoice_item_2 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_1.id, quantity: 15, unit_price: item_2.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       invoice_item_3 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_1.id, quantity: 15, unit_price: item_3.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+       discount_1 = Discount.create!(percent: 20, threshold: 10, merchant_id: merchant_1.id)
+       discount_2 = Discount.create!(percent: 30, threshold: 15, merchant_id: merchant_1.id)
+
+       visit "/merchants/#{merchant_1.id}/invoices/#{invoice_1.id}"
+
+       expect(page).to have_content("Total Revenue: $1650")
+       expect(page).to have_content("Total Discounts: $240")
+       expect(page).to have_content("Net Revenue: $1410")
+     end
 
 end
