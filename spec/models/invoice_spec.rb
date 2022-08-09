7ef644c3d5ce26cc9df5cfd0fb1a4d1100e46bd3
Drew MacNicholas <dmacnicholas@gmail.com>
2022-08-09 10:32:32 -0500

    Add net revenue test

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -117,7 +117,7 @@ RSpec.describe Invoice, type: :model do
       expect(invoice_1.total_price).to eq(12000)
   end
 
-describe 'discounted revenue' do
+describe 'total discounts' do
     it 'calculates that a bulk discount should not be applied to any items ' do
       merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
       item_1 = Item.create!(name: "Watch", description: "Always a need to tell time", unit_price: 30, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
@@ -130,7 +130,7 @@ describe 'discounted revenue' do
       invoice_item_3 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_1.id, quantity: 5, unit_price: item_3.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
       discount_1 = Discount.create!(percent: 20, threshold: 10, merchant_id: merchant_1.id)
 
-      expect(invoice_1.discounted_revenue).to eq(0)
+      expect(invoice_1.total_discount).to eq(0)
     end
 
     it 'calculates that a bulk discount should be applied to some items but not all items' do
@@ -145,7 +145,7 @@ describe 'discounted revenue' do
       invoice_item_3 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_1.id, quantity: 5, unit_price: item_3.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
       discount_1 = Discount.create!(percent: 20, threshold: 10, merchant_id: merchant_1.id)
 
-      expect(invoice_1.discounted_revenue).to eq(60)
+      expect(invoice_1.total_discount).to eq(60)
     end
 
     it 'calculates that multiple bulk discounts should be applied to different items' do
@@ -161,7 +161,7 @@ describe 'discounted revenue' do
       discount_1 = Discount.create!(percent: 20, threshold: 10, merchant_id: merchant_1.id)
       discount_2 = Discount.create!(percent: 30, threshold: 15, merchant_id: merchant_1.id)
 
-      expect(invoice_1.discounted_revenue).to eq(240)
+      expect(invoice_1.total_discount).to eq(240)
     end
 
     it 'calculates that multiple bulk discounts but uses the one with the higher percentage regardless of threshold' do
@@ -177,7 +177,7 @@ describe 'discounted revenue' do
       discount_1 = Discount.create!(percent: 20, threshold: 10, merchant_id: merchant_1.id)
       discount_2 = Discount.create!(percent: 15, threshold: 15, merchant_id: merchant_1.id)
 
-      expect(invoice_1.discounted_revenue).to eq(180)
+      expect(invoice_1.total_discount).to eq(180)
     end
 
     it 'calculates that multiple bulk discounts for multiple merchants' do
@@ -194,7 +194,24 @@ describe 'discounted revenue' do
       discount_1 = Discount.create!(percent: 20, threshold: 10, merchant_id: merchant_1.id)
       discount_2 = Discount.create!(percent: 30, threshold: 15, merchant_id: merchant_1.id)
 
-      expect(invoice_1.discounted_revenue).to eq(240)
+      expect(invoice_1.total_discount).to eq(240)
+    end
+
+    it 'calculates the net revenue of an invoice' do
+      merchant_1 = Merchant.create!(name: "Schroeder-Jerde", created_at: Time.now, updated_at: Time.now)
+      merchant_2 = Merchant.create!(name: "Revtrics", created_at: Time.now, updated_at: Time.now)
+      item_1 = Item.create!(name: "Watch", description: "Always a need to tell time", unit_price: 30, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+      item_2 = Item.create!(name: "Crocs", description: "Worst and Best Shoes", unit_price: 40, merchant_id: merchant_1.id, created_at: Time.now, updated_at: Time.now)
+      item_3 = Item.create!(name: "Beanie", description: "Perfect for a cold day", unit_price: 50, merchant_id: merchant_2.id, created_at: Time.now, updated_at: Time.now)
+      customer_1 = Customer.create!(first_name: "James", last_name: "Franco", created_at: Time.now, updated_at: Time.now)
+      invoice_1 = customer_1.invoices.create!(status: 1, created_at: Time.now, updated_at: Time.now)
+      invoice_item_1 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 10, unit_price: item_1.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+      invoice_item_2 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_1.id, quantity: 15, unit_price: item_2.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+      invoice_item_3 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_1.id, quantity: 15, unit_price: item_3.unit_price, status: 2, created_at: Time.now, updated_at: Time.now)
+      discount_1 = Discount.create!(percent: 20, threshold: 10, merchant_id: merchant_1.id)
+      discount_2 = Discount.create!(percent: 30, threshold: 15, merchant_id: merchant_1.id)
+
+      expect(invoice_1.net_revenue).to eq(1410)
     end
   end
 end
